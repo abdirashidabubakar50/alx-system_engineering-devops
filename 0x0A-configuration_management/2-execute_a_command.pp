d09c65c1762b449f30b57b1417e207426d80552e
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-07-04 02:03:40 +0300

    puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,8 @@
 # This Puppet manifest kills a process named 'killmenow' using pkill.
 
-exec { 'killmenow':
+exec { 'kill_killmenow_process':
   command     => '/usr/bin/pkill -f killmenow',
-  refreshonly => true,
+  path        => ['/usr/bin', '/bin', '/sbin', '/usr/sbin'],
+  onlyif      => '/usr/bin/pgrep -fl killmenow',
+  refreshonly => false, # Changed to false to ensure the command runs without needing a refresh event
 }
