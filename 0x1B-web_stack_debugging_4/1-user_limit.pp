3ced5c89cfc01cea835aa80759bb0324e0749a4b
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-08-24 21:11:15 +0300

    user limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,47 +1,48 @@
-# Increase the number of open files limit for all users
+# Ensure file limits are set for all users
 file { '/etc/security/limits.conf':
   ensure  => file,
   content => "/*\nsoft nofile 65536\nhard nofile 65536\n",
-  notify  => Exec['reload-limits'],
+  notify  => Exec['reload_limits'],
 }
 
-# Ensure PAM limits are applied
+# Ensure PAM limits are applied for interactive sessions
 file { '/etc/pam.d/common-session':
   ensure  => file,
   content => "session required pam_limits.so\n",
-  notify  => Exec['reload-pam'],
+  notify  => Exec['reload_pam'],
 }
 
+# Ensure PAM limits are applied for non-interactive sessions
 file { '/etc/pam.d/common-session-noninteractive':
   ensure  => file,
   content => "session required pam_limits.so\n",
-  notify  => Exec['reload-pam'],
+  notify  => Exec['reload_pam'],
 }
 
 # Set system-wide file descriptor limit
 file { '/etc/sysctl.conf':
   ensure  => file,
   content => "fs.file-max = 65536\n",
-  notify  => Exec['apply-sysctl'],
+  notify  => Exec['apply_sysctl'],
 }
 
-# Apply system configuration
-exec { 'apply-sysctl':
-  command => '/sbin/sysctl -p',
-  path    => ['/sbin', '/bin'],
+# Apply system configuration changes
+exec { 'apply_sysctl':
+  command     => '/sbin/sysctl -p',
+  path        => ['/sbin', '/bin'],
   refreshonly => true,
 }
 
-# Reload PAM settings
-exec { 'reload-pam':
-  command => '/sbin/pam-auth-update --force',
-  path    => ['/sbin', '/bin'],
+# Reload PAM settings to apply limits
+exec { 'reload_pam':
+  command     => '/sbin/pam-auth-update --force',
+  path        => ['/sbin', '/bin'],
   refreshonly => true,
 }
 
-# Reload limits configuration
-exec { 'reload-limits':
-  command => '/sbin/pam-auth-update --force',
-  path    => ['/sbin', '/bin'],
+# Reload limits configuration to apply changes
+exec { 'reload_limits':
+  command     => '/sbin/pam-auth-update --force',
+  path        => ['/sbin', '/bin'],
   refreshonly => true,
 }
