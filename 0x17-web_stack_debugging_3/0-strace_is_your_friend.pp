b6d490c96cee402a2f376f95f848525e9e724924
ABDIRASHID <abdirashidabubakar7@gmail.com>
2024-08-24 21:57:40 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,19 +1,6 @@
-# 0-strace_is_your_friend.pp
-# This Puppet manifest fixes the WordPress wp-settings.php file by replacing
-# 'phpp' with 'php' to correct any typos in the file.
+phpp" extensions to "php" in "wp-settings.php".
 
-exec { 'fix-wordpress':
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path    => '/usr/bin:/bin',
-  user    => 'root',
-  group   => 'root',
-  unless  => 'grep -q "phpp" /var/www/html/wp-settings.php',
-  require => File['/var/www/html/wp-settings.php'],
-}
-
-file { '/var/www/html/wp-settings.php':
-  ensure => file,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0644',
+exec{'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr'
 }
